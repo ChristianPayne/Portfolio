f4c497400597775ed2fc31c969b679c5d510df09
Christian Payne <christianpayne522@gmail.com>
2021-01-28 13:37:16 -0800

    Added tailwindcss.

---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg bg-red-500 mb-3">
+      <a href="/">Home</a>
+      <a href="/">Projects</a>
+      <a href="/">Contact Me</a>
+      <a href="/">Skills</a>
+      <a href="/">Links</a>
+
+      
+      </nav>
+  );
+}
+
+export default Navbar;
